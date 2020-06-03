3e2aa381289937a9ef8a4b825b9f2824386f57d5
Rashid <rabenkralle@yandex.ru>
2020-06-03 21:37:08 +0300

    'lesson_7_done'

--- lesson_7_selenium/mvideoparser.py
+++ lesson_7_selenium/mvideoparser.py
@@ -1,41 +1,51 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from pymongo import MongoClient
+import json
 import time
 
 client = MongoClient('localhost', 27017)        #Подключается БД
 db = client['mvideo_top_db']
 top_sales = db.mvideo
 
-chrome_options = Options()
+chrome_options = Options()                      #Выставляем опции драйвера и запускаем
 chrome_options.add_argument('start-maximized')
 # chrome_options.add_argument('--headless')
 driver = webdriver.Chrome(options=chrome_options)
 
-driver.get('https://www.mvideo.ru/')
+driver.get('https://www.mvideo.ru/')            #Входим на сайт Мвидео
 time.sleep(5)
 assert "М.Видео" in driver.title
-elem = driver.find_elements_by_xpath('//div[@class="gallery-layout"]')
+
+elem = driver.find_elements_by_xpath('//div[@class="gallery-layout"]') #Ищем таблицы с товарами и спускаем по странице к нужному элементу
 actions = ActionChains(driver)
 actions.move_to_element(elem[2])
 actions.perform()
-button = driver.find_element_by_xpath('//div[@class="gallery-layout"][2]//div[contains(@class, "sel-hits-block")]//a[@class = "next-btn sel-hits-button-next"]')
 
-while True:
-    actions.move_to_element(button).click().perform()
+while True:                         #Двигаемся к кнопке далее и нажимаем ее, если она есть
     try:
         button = driver.find_element_by_xpath(
             '//div[@class="gallery-layout"][2]//div[contains(@class, "sel-hits-block")]//a[@class = "next-btn sel-hits-button-next"]')
     except:
         break
-elem = driver.find_elements_by_xpath('//div[@class="gallery-layout"][2]//div[contains(@class, "sel-hits-block")]//li[@class="gallery-list-item height-ready"]//a[@data-product-info]')
-for el in elem:
-    el = el.get_attribute('data-product-info')
-    top_sales.insert_many(el)
+    actions.move_to_element(button).click().perform()
+
+
+
+items = driver.find_elements_by_xpath('//div[@class="gallery-layout"][2]//div[contains(@class, "sel-hits-block")]//li[@class="gallery-list-item height-ready"]//h4/a[@data-product-info]')
+
+#Собираем все появившиеся товары
+
+for item in items:      #Перебираем товары и закидываем в базу данных
+    item = item.get_attribute('data-product-info')
+    item = item.replace('\n', '')
+    item = item.replace('\t','')
+    item = json.loads(item)
+    top_sales.insert_one(item)
+
+
+
+
 
 driver.quit()
