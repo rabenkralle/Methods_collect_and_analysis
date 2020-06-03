4b8b3c1dd4729b452d43d86d832c4e1ef4766219
Rashid <rabenkralle@yandex.ru>
2020-06-03 18:02:00 +0300

    'lesson_7_mvideo_start'

--- lesson_7_selenium/mvideoparser.py
+++ lesson_7_selenium/mvideoparser.py
@@ -20,13 +20,6 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get('https://www.mvideo.ru/')
 time.sleep(5)
 assert "М.Видео" in driver.title
-try:
-    while True:
-        button = driver.find_elements_by_xpath('//div[@class="gallery-layout"][2]//div[contains(@class, "sel-hits-block")]//a[@class = "next-btn sel-hits-button-next"]')
-        button.click()
-except:
-    pass
-
 elem = driver.find_elements_by_xpath('//div[@class="gallery-layout"]')
 actions = ActionChains(driver)
 actions.move_to_element(elem[2])
@@ -35,11 +28,14 @@ button = driver.find_element_by_xpath('//div[@class="gallery-layout"][2]//div[co
 
 while True:
     actions.move_to_element(button).click().perform()
-
-
+    try:
+        button = driver.find_element_by_xpath(
+            '//div[@class="gallery-layout"][2]//div[contains(@class, "sel-hits-block")]//a[@class = "next-btn sel-hits-button-next"]')
+    except:
+        break
 elem = driver.find_elements_by_xpath('//div[@class="gallery-layout"][2]//div[contains(@class, "sel-hits-block")]//li[@class="gallery-list-item height-ready"]//a[@data-product-info]')
 for el in elem:
     el = el.get_attribute('data-product-info')
-    top_sale = insert_one(el)
+    top_sales.insert_many(el)
 
-
+driver.quit()
